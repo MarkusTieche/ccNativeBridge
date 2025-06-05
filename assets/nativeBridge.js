fa592c1d4fc3110d9e5ba266175dc8688e3f973e
MarkusTieche <tieche@gmx.com>
2025-06-05 10:24:09 +0200

    removed unused

--- assets/nativeBridge.js
+++ assets/nativeBridge.js
@@ -1,14 +1,7 @@
 window.nativeBridge = {
 
-    adFrequency : 3,
-    lastAdShown : Date.now(),
-    
     load ()
     {
-        this.callbackFunc = null;
-        this.adCounter = this.adFrequency,
-        cc.log("nativeBridgeLoaded")
-
         //CALLBACK EVENTS
         cc.systemEvent.on('interstitial-ad-closed', function () {
             console.log("Interstitial ad closed! Handling global event.");
@@ -21,18 +14,6 @@ window.nativeBridge = {
         }, this);
     },
 
-    timeElapsed(time)
-    {
-        var timeElapsed =  Math.floor((new Date() - this.lastAdShown)/1000); //IN SECONDS
-        //SHOW NEXT AD ONLY AFTER time in s
-        cc.log(time,"timeElapsed: "+timeElapsed)
-        if(timeElapsed >= time)
-        {
-            return true;
-        }
-        return false;
-    },
-
     requestConsent()
     {
         if (cc.sys.isNative) {
@@ -43,17 +24,10 @@ window.nativeBridge = {
 
     showBanner()
     {
-        //SHOW BANNER AFTER PRELOAD SCREEN :)
         if(cc.sys.isNative)
         {   
-            if(cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("adController","showBanner");
-            }
-
-            if(cc.sys.platform == cc.sys.ANDROID)
-            {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "showBanner","()V");
-            }
+            if(cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("adController","showBanner"); }
+            if(cc.sys.platform == cc.sys.ANDROID){ jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "showBanner","()V"); }
         }
     },
 
@@ -61,13 +35,8 @@ window.nativeBridge = {
     {
         if (cc.sys.isNative)
         {
-            if (cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("adController", "hideBanner");
-            }
-
-            if (cc.sys.platform == cc.sys.ANDROID) {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "hideBanner", "()V");
-            }
+            if (cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("adController", "hideBanner"); }
+            if (cc.sys.platform == cc.sys.ANDROID) { jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "hideBanner", "()V"); }
         }
     },
 
@@ -75,14 +44,8 @@ window.nativeBridge = {
     {
         if(cc.sys.isNative)
         {
-            if(cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("appRating","requestAppReview");
-            }
-
-            if(cc.sys.platform == cc.sys.ANDROID)
-            {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/appRating", "requestAppReview","()V");
-            }
+            if(cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("appRating","requestAppReview"); }
+            if(cc.sys.platform == cc.sys.ANDROID){ jsb.reflection.callStaticMethod("org/cocos2dx/javascript/appRating", "requestAppReview","()V"); }
         }
     },
 
@@ -90,15 +53,8 @@ window.nativeBridge = {
     {
         if(cc.sys.isNative)
         {
-            if(cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("gameCenter","showLeaderboard");
-            }
-
-            if(cc.sys.platform == cc.sys.ANDROID)
-            {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/AppActivity", "showLeaderboard","()V");
-                // jsb.reflection.callStaticMethod("org/cocos2dx/javascript/googleService", "showLeaderboard","()V");
-            }
+            if(cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("gameCenter","showLeaderboard"); }
+            if(cc.sys.platform == cc.sys.ANDROID){ jsb.reflection.callStaticMethod("org/cocos2dx/javascript/AppActivity", "showLeaderboard","()V"); }
         }
     },
     
@@ -106,85 +62,45 @@ window.nativeBridge = {
     {
         if(cc.sys.isNative)
         {
-            if(cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("gameCenter","submitScore:toLeaderboard:",score,"speedwings_leaderboard");
-            }
-            
-            if(cc.sys.platform == cc.sys.ANDROID)
-            {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/AppActivity", "submitScore","(I)V",score);
-            }
+            if(cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("gameCenter","submitScore:toLeaderboard:",score,"speedwings_leaderboard"); }
+            if(cc.sys.platform == cc.sys.ANDROID){ jsb.reflection.callStaticMethod("org/cocos2dx/javascript/AppActivity", "submitScore","(I)V",score); }
         }
     },
 
     interstitialAvailable()
     {
         if (cc.sys.isNative) {
-            if (cc.sys.os == cc.sys.OS_IOS) {
-                return jsb.reflection.callStaticMethod("adController", "interstitialAvailable");
-                
-            }
-
-            if (cc.sys.platform == cc.sys.ANDROID) {
-
-                return jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "interstitialAvailable", "()Z");
-            }
+            if (cc.sys.os == cc.sys.OS_IOS) { return jsb.reflection.callStaticMethod("adController", "interstitialAvailable"); }
+            if (cc.sys.platform == cc.sys.ANDROID) { return jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "interstitialAvailable", "()Z");}
         }
     },
 
-    showInterstitial(callFunc)
+    showInterstitial()
     {
-        // if(!this.timeElapsed(180)){return};
-
         if(cc.sys.isNative)
         {
-            if(cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("adController","showInterstitial");
-            }
-            
-            if(cc.sys.platform == cc.sys.ANDROID)
-            {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "showInterstitial","()V");
-            }
+            if(cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("adController","showInterstitial");}
+            if(cc.sys.platform == cc.sys.ANDROID){ jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "showInterstitial","()V");}
         } 
-
     },
     
-    showRewardVideo(callFunc)
-    {
-        this.callbackFunc = callFunc;
-        this.lastAdShown = Date.now();
-
-        if(cc.sys.isNative)
-        {
-            if(cc.sys.os == cc.sys.OS_IOS) {
-                jsb.reflection.callStaticMethod("adController","showRewardedAd");
-            }
-
-            if(cc.sys.platform == cc.sys.ANDROID)
-            {
-                jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "showRewardedAd","()V");
-            }
-
-        }
-            
-        //this.rewardClosed();
-
-    },
-
     rewardVideoAvailable()
     {
         if (cc.sys.isNative) {
-            if (cc.sys.os == cc.sys.OS_IOS) {
-                return jsb.reflection.callStaticMethod("adController", "rewardVideoAvailable");
-            }
-
-            if (cc.sys.platform == cc.sys.ANDROID) {
-
-                return jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "rewardVideoAvailable", "()Z");
-            }
+            if (cc.sys.os == cc.sys.OS_IOS) { return jsb.reflection.callStaticMethod("adController", "rewardVideoAvailable");}
+            if (cc.sys.platform == cc.sys.ANDROID) { return jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "rewardVideoAvailable", "()Z");}
         }
     },
+
+    showRewardVideo()
+    {
+        if(cc.sys.isNative)
+        {
+            if(cc.sys.os == cc.sys.OS_IOS) { jsb.reflection.callStaticMethod("adController","showRewardedAd");}
+            if(cc.sys.platform == cc.sys.ANDROID){ jsb.reflection.callStaticMethod("org/cocos2dx/javascript/adController", "showRewardedAd","()V");}
+        }
+    },
+
 };
 
 if(!CC_EDITOR)
